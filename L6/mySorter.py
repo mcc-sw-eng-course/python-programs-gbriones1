e1b454bd56f1f5bcd65846ce304aa79882a22486
Gabriel Briones <gabriel.briones.sayeg@intel.com>
2019-03-05 19:50:36 -0600

    Add heap sort to MySorter
    Signed-off-by: Gabriel Briones <gabriel.briones.sayeg@intel.com>

--- L6/mySorter.py
+++ L6/mySorter.py
@@ -1,10 +1,14 @@
 import csv
+import time
 
 class mySorter():
     """ CLASS THAT HANDLES CSV FILES AND IMPLEMENTS METHODS FOR SORTING THEIR CONTENT"""
     
     def __init__(self, alist = None):
         self.alist = alist
+        self.records_sorted = 0
+        self.start_time = None
+        self.end_time = None
         
     def set_input_data(self, file_path_name: str):
         """This methods sets the information about the file that will be used to read the data"""
@@ -29,23 +33,57 @@ class mySorter():
             filewriter = csv.writer(csvfile, delimiter=',')
             filewriter.writerow(self.alist)
     
-    def execute_merge_sort(self, arr):
-        """This methods sorts the data contained in the file specified"""
-        pass
-
-    
     def execute_heap_sort(self):
         """This methods sorts the data contained in the file specified"""
-        pass
+        self.records_sorted = len(self.alist)
+        self.start_time = time.time()
+        arr = self.alist
+        n = len(arr) 
+    
+        # Build a maxheap. 
+        for i in range(n, -1, -1): 
+            self._heap_sort_recursion(arr, n, i) 
+    
+        # One by one extract elements 
+        for i in range(n-1, 0, -1): 
+            arr[i], arr[0] = arr[0], arr[i] # swap 
+            self._heap_sort_recursion(arr, i, 0)
+        self.end_time = time.time()
+
+    def _heap_sort_recursion(self, arr, n, i):
+        # Find largest among root and children
+        largest = i
+        l = 2 * i + 1
+        r = 2 * i + 2 
+        if l < n and arr[i] < arr[l]:
+            largest = l
+        if r < n and arr[largest] < arr[r]:
+            largest = r
+        # If root is not largest, swap with largest and continue heapifying
+        if largest != i:
+            arr[i],arr[largest] = arr[largest],arr[i]
+            self._heap_sort_recursion(arr, n, largest)
     
     def execute_quick_sort(self):
         """This methods sorts the data contained in the file specified"""
-        return self._quick_sort_recursion(self.alist, 0, len(self.alist)-1)
+        self.records_sorted = len(self.alist)
+        self.start_time = time.time()
+        self._quick_sort_recursion(self.alist, 0, len(self.alist)-1)
+        self.end_time = time.time()
     
     def get_performance_data(self):
         """This method returns the performance data associated to the last sorting execution
             [Number of Records Sorted, TimeConsumed, StartTime, EndTime]"""
-        pass
+        time_consumed = None
+        if self.start_time and self.end_time:
+            time_consumed = self.end_time - self.start_time
+        performance_data = {
+            "Records Sorted": self.records_sorted,
+            "Time Consumed": time_consumed,
+            "Start Time": self.start_time,
+            "End Time": self.end_time
+        }
+        return performance_data
     
     def _partition(self, array, begin, end):
         pivot_idx = begin
@@ -64,7 +102,10 @@ class mySorter():
         self._quick_sort_recursion(array, pivot_idx+1, end)
 
     def execute_merge_sort(self):
+        self.records_sorted = len(self.alist)
+        self.start_time = time.time()
         self._merge_sort_recursion(self.alist)
+        self.end_time = time.time()
 
     def _merge_sort_recursion(self, alist):
         #print("Splitting ",alist)
--- L6/test.py
+++ L6/test.py
@@ -23,6 +23,7 @@ class TestMySorter(unittest.TestCase):
         sorter.execute_merge_sort()
         alist = sorted(alist)
         self.assertTrue(sorter.alist == alist)
+        print("Merge sort data", sorter.get_performance_data())
 
     def test_execute_quick_sort(self):
         alist = [5.28,66.24,85.4,4,5.5,8.95,15.4]
@@ -30,6 +31,15 @@ class TestMySorter(unittest.TestCase):
         sorter.execute_quick_sort()
         alist = sorted(alist)
         self.assertTrue(sorter.alist == alist)
+        print("Quick sort data", sorter.get_performance_data())
+
+    def test_execute_heap_sort(self):
+        alist = [5.28,66.24,85.4,4,5.5,8.95,15.4]
+        sorter = mySorter(alist)
+        sorter.execute_heap_sort()
+        alist = sorted(alist)
+        self.assertTrue(sorter.alist == alist)
+        print("Heap sort data", sorter.get_performance_data())
 
     def test_execute_merge_sort_large(self):
         sorter = mySorter()
@@ -37,6 +47,7 @@ class TestMySorter(unittest.TestCase):
         alist = sorted(sorter.alist)
         sorter.execute_merge_sort()
         self.assertTrue(sorter.alist == alist)
+        print("Merge sort large data", sorter.get_performance_data())
 
     def test_execute_quick_sort_large(self):
         sorter = mySorter()
@@ -44,6 +55,15 @@ class TestMySorter(unittest.TestCase):
         alist = sorted(sorter.alist)
         sorter.execute_quick_sort()
         self.assertTrue(sorter.alist == alist)
+        print("Quick sort large data", sorter.get_performance_data())
+
+    def test_execute_heap_sort_large(self):
+        sorter = mySorter()
+        sorter.set_input_data("large.csv")
+        alist = sorted(sorter.alist)
+        sorter.execute_heap_sort()
+        self.assertTrue(sorter.alist == alist)
+        print("Heap sort large data", sorter.get_performance_data())
 
 if __name__ == '__main__':
     unittest.main()
